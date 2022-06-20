31f5bd70e3160843b2dd7915b29d48e3596171ff
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-06-20 15:27:19 +0300

    Add solutions of problems on Recursion and Backtracking topic

---
+++ RecursionAndBacktracking/Lab/04.RecursiveFactorial/04.RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalcFactorialRecursively(n));
+        }
+
+        private static int CalcFactorialRecursively(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * CalcFactorialRecursively(n - 1);
+        }
+    }
+}
