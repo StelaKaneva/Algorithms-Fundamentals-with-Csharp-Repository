828e3f3768f497886939ec6afd70804ebfb8d8a4
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-06-23 09:48:25 +0300

    Add solutions of problems on Recursion and Backtracking topic

---
+++ RecursionAndBacktracking/07.RecursiveFibonacci/07.RecursiveFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07.RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(number));
+        }
+
+        public static int Fibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(number - 2) + Fibonacci(number - 1);
+            }
+        }
+    }
+}
+
