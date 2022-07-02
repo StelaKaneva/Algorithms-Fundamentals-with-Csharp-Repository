94c72e083153b0bb3cef8fcf990ac11b3c3b1d8e
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-07-02 20:40:54 +0300

    Add solutions of problems on Searching, Sorting and Greedy Algorithms topic

---
+++ Searching,SortingAndGreedyAlgorithms/LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(LinearSearch(numbers, 3));
+        }
+
+        private static int LinearSearch(int[] elements, int number)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] == number)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
