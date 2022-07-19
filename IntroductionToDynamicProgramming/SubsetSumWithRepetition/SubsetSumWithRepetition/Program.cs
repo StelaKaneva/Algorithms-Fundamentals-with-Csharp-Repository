bc5a094d5ce0b44e0eae88d13d4e4d8fbf4df899
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-07-19 18:51:05 +0300

    Add solutions of problems on Dynamic Programming topic

---
+++ IntroductionToDynamicProgramming/SubsetSumWithRepetition/SubsetSumWithRepetition/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubsetSumWithRepetition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new[] { 3, 5, 2 };
+            var target = 17;
+
+            var sums = new bool[target + 1];
+            sums[0] = true;
+
+            for (int sum = 0; sum < sums.Length;sum++)
+            {
+                if (!sums[sum])
+                {
+                    continue;
+                }
+
+                foreach (var num in nums)
+                {
+                    var newSum = sum + num;
+
+                    if (newSum > target)
+                    {
+                        continue;
+                    }
+
+                    sums[newSum] = true;
+                }
+            }
+
+            var subset = new List<int>();
+
+            while (target > 0)
+            {
+                foreach (var num in nums)
+                {
+                    var prevSum = target - num;
+
+                    if (prevSum >= 0 && sums[prevSum])
+                    {
+                        subset.Add(num);
+                        target = prevSum;
+
+                        if (target == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", subset));
+        }
+    }
+}
