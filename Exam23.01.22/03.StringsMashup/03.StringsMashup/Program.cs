aab70c70491bf277ede81e94f85cfb593c81ea93
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-07-22 16:03:05 +0300

    Add solutions of problems

---
+++ Exam23.01.22/03.StringsMashup/03.StringsMashup/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.StringsMashup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            IList<string> combinations = LetterCasePermutation(input);
+
+            Console.WriteLine(string.Join(Environment.NewLine, combinations));
+        }
+
+        public static IList<string> LetterCasePermutation(string S)
+        {
+            IList<string> combinations = new List<string>();
+            Helper(S, combinations, "", 0);
+            return combinations;
+        }
+
+        private static void Helper(string s, IList<string> combinations, string curr, int index)
+        {
+            while (curr.Length < s.Length && char.IsDigit(s[index]))
+            {
+                curr += s[index++].ToString();
+            }
+
+            if (curr.Length == s.Length)
+            {
+                combinations.Add(new string(curr));
+                return;
+            }
+
+            string ch = s[index].ToString();
+            
+            Helper(s, combinations, curr + ch.ToUpper(), index + 1);
+            
+            Helper(s, combinations, curr + ch.ToLower(), index + 1);
+        }
+    }
+}
+    
+
