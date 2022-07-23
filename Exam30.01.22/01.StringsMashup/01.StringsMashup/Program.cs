1dfef6643596b0de6e52bc56a36cdfa0cf50a0bf
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-07-23 15:23:52 +0300

    Add solutions of problems

---
+++ Exam30.01.22/01.StringsMashup/01.StringsMashup/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.StringsMashup
+{
+    internal class Program
+    {
+        private static int k;
+        private static char[] elements;
+        private static char[] combinations;
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            k = int.Parse(Console.ReadLine());
+            elements = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                elements[i] = input[i];
+            }
+
+            Array.Sort(elements);
+
+            combinations = new char[k];
+
+            Combinations(0, 0);
+        }
+
+        private static void Combinations(int idx, int elementsStartIndex)
+        {
+            if (idx >= combinations.Length)
+            {
+                Console.WriteLine(String.Join("", combinations));
+                return;
+            }
+
+            for (int i = elementsStartIndex; i < elements.Length; i++)
+            {
+                combinations[idx] = elements[i];
+                Combinations(idx + 1, i);
+            }
+        }
+    }
+}
