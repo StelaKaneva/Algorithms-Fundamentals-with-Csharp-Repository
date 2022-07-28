bbcd6a7b81a858cd950814666c875cdab66a1d9f
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-07-28 12:34:33 +0300

    Add solutions of problems

---
+++ Exam24.07.22/02.ConditionalExpressionResolver/02.ConditionalExpressionResolver/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02.ConditionalExpressionResolver
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            line = string.Join("", line.Split());
+            Resolve(line);
+        }
+
+        private static void Resolve(string line)
+        {
+            int innerOperatorStart = line.LastIndexOfAny(new char[] { 't', 'f' });
+            
+            if (innerOperatorStart == 0)
+            {
+                if (line[innerOperatorStart] == 't')
+                {
+                    Console.WriteLine(line[2]);
+                    return;
+                }
+                Console.WriteLine(line[4]);
+                return;
+            }
+
+            string result;
+
+            if (line[innerOperatorStart] == 't')
+            {
+                result = line[innerOperatorStart + 2].ToString();
+
+            }
+            else 
+            { 
+                result = line[innerOperatorStart + 4].ToString(); 
+            }
+
+            line = line.Remove(innerOperatorStart, 5);
+            line = line.Insert(innerOperatorStart, result);
+            Resolve(line);
+        }
+    }
+}
